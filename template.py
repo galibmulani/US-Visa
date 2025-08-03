103bb0f388494758f05908f1dd7fa7e801d338f0
galibmulani <mulanigalib1@gmail.com>
2025-08-03 23:14:12 +0530

    project structure added

---
+++ template.py
@@ -0,0 +1,50 @@
+import os 
+from pathlib import Path
+
+project_name = "us-visa"
+
+
+list_of_files = [
+    
+    f"{project_name}/__init__.py",
+    f"{project_name}/components/__init__.py",
+    f"{project_name}/components/data_ingestion.py",  
+    f"{project_name}/components/data_validation.py",
+    f"{project_name}/components/data_transformation.py",
+    f"{project_name}/components/model_trainer.py",
+    f"{project_name}/components/model_evaluation.py",
+    f"{project_name}/components/model_pusher.py",
+    f"{project_name}/configuration/__init__.py",
+    f"{project_name}/constants/__init__.py",
+    f"{project_name}/entity/__init__.py",
+    f"{project_name}/entity/config_entity.py",
+    f"{project_name}/entity/artifact_entity.py",
+    f"{project_name}/exception/__init__.py",
+    f"{project_name}/logger/__init__.py",
+    f"{project_name}/pipline/__init__.py",
+    f"{project_name}/pipline/training_pipeline.py",
+    f"{project_name}/pipline/prediction_pipeline.py",
+    f"{project_name}/utils/__init__.py",
+    f"{project_name}/utils/main_utils.py",
+    "app.py",
+    "requirements.txt",
+    "Dockerfile",
+    ".dockerignore",
+    "demo.py",
+    "setup.py",
+    "config/model.yaml",
+    "config/schema.yaml",
+]
+
+
+for file_path in list_of_files:
+    file_path = Path(file_path)
+    file_dir = file_path.parent
+
+    if not file_dir.exists():
+        os.makedirs(file_dir, exist_ok=True)
+    if (not file_path.exists() or file_path.stat().st_size == 0):
+        with open(file_path, "w") as f:
+            pass
+    else:
+        print(f"File {file_path} already exists")
